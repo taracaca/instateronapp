d223bec0e3db5a8cb81d1a13c2130a2cd0e2c379
taracaca <fernando_levy_assis@hotmail.com>
2018-10-31 15:22:22 -0300

    segundo commit
    deletei tudo

--- app/src/main/java/com/myapps/upesse/upes_spefest/camera/AlbumStorageDirFactory.java
+++
@@ -1,7 +0,0 @@
-package com.myapps.upesse.upes_spefest.camera;
-
-import java.io.File;
-
-public abstract class AlbumStorageDirFactory {
-	public abstract File getAlbumStorageDir(String albumName);
-}
